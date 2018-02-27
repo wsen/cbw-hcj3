16148b20b3161f6b4900c80b41eea586aebdf650
w_sen <w_sen@flashbox.de>
2018-02-27 23:58:03 +0100

    0170227 (casa): Udacity Ajax with XHR app.js

---
+++ 00_app.js
@@ -0,0 +1,11 @@
+const searchedForText = 'hippos';
+const unsplashRequest = new XMLHttpRequest();
+
+unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+unsplashRequest.onload = addImage;
+unsplashRequest.setRequestHeader('Authorization', 'Client-ID <your-client-id>');
+unsplashRequest.send();
+
+function addImage(){
+    debugger;
+}
